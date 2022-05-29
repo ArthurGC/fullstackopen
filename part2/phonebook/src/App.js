40514e054a83675fd09c271822a622f754967054
Alex Guzman <aguzman@buk.pe>
2022-05-29 15:14:42 -0500

    feat:Add phonebook part1

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { nanoid } from 'nanoid'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleName = (event) => setNewName(event.target.value)
+  const AddPerson = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={AddPerson}>
+        <div>
+          name: <input value={newName} onChange={handleName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) =>{
+        return <p key={nanoid()}>{person.name}</p>
+      })}
+    </div>
+  )
+}
+
+export default App
