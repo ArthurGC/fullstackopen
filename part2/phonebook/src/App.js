fd333852fb01c7ca2c162fcba54873f8c1b17dd6
Alex Guzman <aguzman@buk.pe>
2022-05-29 16:50:05 -0500

    feat:Add phonebook part3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,23 +2,31 @@ import React, { useState } from 'react'
 import { nanoid } from 'nanoid'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '959854575' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleName = (event) => setNewName(event.target.value)
+  const handleNumber = (event) => setNewNumber(event.target.value)
+
   const AddPerson = (event) => {
     event.preventDefault();
+    let isEmpty = newName.trim() && newNumber.trim()
     let isRepeated = persons.find((person) => person.name === newName)
-    if (isRepeated) {
+    if (!isEmpty) {
+      alert('Fields are empty!')
+    } else if (isRepeated) {
       alert(`${newName} is already added to phonebook`)
     } else {
       const newPerson = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -30,12 +38,15 @@ const App = () => {
           name: <input value={newName} onChange={handleName} />
         </div>
         <div>
+          number: <input type="number" value={newNumber} onChange={handleNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) =>{
-        return <p key={nanoid()}>{person.name}</p>
+      {persons.map((person) => {
+        return <p key={nanoid()}>{person.name} {person.number}</p>
       })}
     </div>
   )
