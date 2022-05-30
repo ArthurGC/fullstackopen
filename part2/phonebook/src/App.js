76f3dd9a9c0ed2d49708ee5c70769530c9a8f6c1
Alex Guzman <aguzman@buk.pe>
2022-05-29 19:20:15 -0500

    feat:Add phonebook part4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,13 +3,22 @@ import { nanoid } from 'nanoid'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '959854575' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [wordFiltered, setWordFiltered] = useState('')
+
+  const personsToShow = wordFiltered.trim() 
+    ? persons.filter((person) => person.name.toLowerCase().includes(wordFiltered.toLowerCase()))
+    : persons
 
   const handleName = (event) => setNewName(event.target.value)
   const handleNumber = (event) => setNewNumber(event.target.value)
+  const handleFilter = (event) => setWordFiltered(event.target.value)
 
   const AddPerson = (event) => {
     event.preventDefault();
@@ -33,6 +42,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with: <input value={wordFiltered} onChange={handleFilter}/>
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={AddPerson}>
         <div>
           name: <input value={newName} onChange={handleName} />
@@ -45,7 +58,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => {
+      {personsToShow.map((person) => {
         return <p key={nanoid()}>{person.name} {person.number}</p>
       })}
     </div>
